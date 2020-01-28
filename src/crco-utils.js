263aead1e839c4f27586b63f0126f62081390b5d
Mark Racette <markracette@gmail.com>
2020-01-28 07:56:20 -0500

    export from audio folder

--- src/crco-utils.js
+++ src/crco-utils.js
@@ -4,6 +4,7 @@ export * from './utils/constants';
 export * from './utils/math';
 export * from './utils/canvas';
 export * from './utils/geometry';
+export * from './utils/audio';
 
 export { Spread } from './classes/Spread';
 export { CanvasCoordinates } from './classes/CanvasCoordinates';

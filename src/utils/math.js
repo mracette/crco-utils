8fd7c42bbb340c1c3fa5920fdbb76a591a70ceb1
Mark Racette <markracette@gmail.com>
2020-03-01 20:42:03 -0500

    gaussian rand

--- src/utils/math.js
+++ src/utils/math.js
@@ -80,3 +80,26 @@ export const linToLog = (w) => {
 
     return { a, b };
 }
+
+/**
+ * Approximates a [0, 1] gaussian distribution and returns a random number from that distribution.
+ * @param {number} [factor = 6] The higher the factor, the more closely the distribution resembles a Gaussian distribution. Tradeoff is speed.
+ */
+export const gaussianRand = (factor = 6) => {
+    const rand = 0;
+    for (let i = 0; i < factor; i += 1) {
+        rand += Math.random();
+    }
+    return rand / factor;
+}
+
+export const normalize = (x, y, z, r) => {
+    const nX = r * x / Math.sqrt(x * x + y * y + z * z)
+    const nY = r * y / Math.sqrt(x * x + y * y + z * z)
+    const nZ = r * z / Math.sqrt(x * x + y * y + z * z)
+    return {
+        x: nX,
+        y: nY,
+        z: nZ
+    };
+}

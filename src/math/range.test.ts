e9f8c0d7b04760fb2679a216f3a8b43d46a2632f
mracette <markracette@gmail.com>
2022-07-10 20:02:55 -0500

    add range

---
+++ src/math/range.test.ts
@@ -0,0 +1,10 @@
+import { range } from "./range";
+
+describe("range", () => {
+  test("positive step", () => {
+    expect(range(0, 1, 0.5)).toStrictEqual([0, 0.5, 1]);
+  });
+  test("negative step", () => {
+    expect(range(1, 0, -0.5)).toStrictEqual([1, 0.5, 0]);
+  });
+});

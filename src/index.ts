b71601fbdbd51543ae1037227535097ee913b7bc
mracette <markracette@gmail.com>
2022-07-30 17:25:18 -0500

    update exports

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,8 @@ export * from './math/lerp';
 export * from './math/normalize';
 export * from './math/Vector2';
 export * from './dom/aspectRatioResize';
+export * from './geometry/equilateralTriangle';
+export * from './geometry/isocelesTriangle';
+export * from './geometry/polygon';
+export * from './geometry/rotatePoint';
+export * from './geometry/star';

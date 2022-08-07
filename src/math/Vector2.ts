43c1e7ca9075feef186a8d25fb4a0358a44bcc05
mracette <markracette@gmail.com>
2022-08-07 13:33:16 -0500

    vector methods

--- src/math/Vector2.ts
+++ src/math/Vector2.ts
@@ -1,7 +1,20 @@
 export class Vector2 {
   constructor(public x: number, public y: number) {}
 
-  ratio(): number {
+  get ratio(): number {
     return this.x / this.y;
+  }
+
+  get magnitude(): number {
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+  }
+
+  toUnitVector(): Vector2 {
+    const magnitude = this.magnitude;
+    return new Vector2(this.x / magnitude, this.y / magnitude);
+  }
+
+  static from(a: Vector2, b: Vector2) {
+    return new Vector2(a.x - b.x, a.y - b.y);
   }
 }

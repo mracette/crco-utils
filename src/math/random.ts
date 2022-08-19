0f754e14d70d86b121f59d2c79dea37119d5fc2a
mracette <markracette@gmail.com>
2022-08-19 09:07:05 -0500

    overload random

--- src/math/random.ts
+++ src/math/random.ts
@@ -1,1 +1,12 @@
-export const random = (max = 1, min = 0) => min + Math.random() * (max - min);
+export function random(max?: number, min?: number): number;
+export function random<T>(options: T[]): T;
+export function random<T>(
+  optionsOrMax: T[] | number | undefined = 0,
+  min = 1
+): T | number {
+  if (Array.isArray(optionsOrMax)) {
+    return optionsOrMax[Math.floor(Math.random() * optionsOrMax.length)];
+  } else {
+    return min + Math.random() * (optionsOrMax - min);
+  }
+}
